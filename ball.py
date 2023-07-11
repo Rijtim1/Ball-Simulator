4618a02c55b99755cefc4df40ee2c2c320ef7c6d
Rijan Timsina <regiontimsina@gmail.com>
2023-07-11 18:01:59 -0400

    random bounce gain velocity

--- ball.py
+++ ball.py
@@ -22,12 +22,26 @@ class Ball:
             self.position = (self.position[0], height - self.radius)
             self.is_squished = True
 
+            if random.random() < 0.2:  # Adjust the probability as desired
+                extra_velocity = random.uniform(0.2, 0.5)  # Adjust the range as desired
+                self.velocity = (
+                    self.velocity[0] * (1 + extra_velocity),
+                    self.velocity[1] * (1 + extra_velocity)
+                )
+
         if self.position[1] - self.radius <= 0:
             self.velocity = (
                 self.velocity[0] * self.damping + random.uniform(-0.2, 0.2),
                 -self.velocity[1] * self.damping + random.uniform(-0.2, 0.2)
             )
             self.position = (self.position[0], self.radius)
+
+            if random.random() < 0.2:
+                extra_velocity = random.uniform(0.2, 0.5)
+                self.velocity = (
+                    self.velocity[0] * (1 + extra_velocity),
+                    self.velocity[1] * (1 + extra_velocity)
+                )
 
         if self.position[0] - self.radius <= 0:
             self.velocity = (
@@ -36,12 +50,27 @@ class Ball:
             )
             self.position = (self.radius, self.position[1])
 
+            if random.random() < 0.2:
+                extra_velocity = random.uniform(0.2, 0.5)
+                self.velocity = (
+                    self.velocity[0] * (1 + extra_velocity),
+                    self.velocity[1] * (1 + extra_velocity)
+                )
+
         if self.position[0] + self.radius >= width:
             self.velocity = (
                 -self.velocity[0] * self.damping + random.uniform(-0.2, 0.2),
                 self.velocity[1] * self.damping + random.uniform(-0.2, 0.2)
             )
             self.position = (width - self.radius, self.position[1])
+
+            if random.random() < 0.2:
+                extra_velocity = random.uniform(0.2, 0.5)
+                self.velocity = (
+                    self.velocity[0] * (1 + extra_velocity),
+                    self.velocity[1] * (1 + extra_velocity)
+                )
+
 
     def draw(self, surface: pygame.Surface):
         pygame.draw.circle(surface, self.color, (int(self.position[0]), int(self.position[1])), int(self.radius))
